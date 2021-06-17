fa545ed32de345e41200792107e3adfce8e0a37f
layfu <60004939+layfu@users.noreply.github.com>
2021-06-17 21:55:58 +0800

    Update jingweixiangji.js

--- jingweixiangji.js
+++ jingweixiangji.js
@@ -1,7 +1,7 @@
 /*
 yuandaojingweixiangji Chain breaker
 QX:
-https:\/\/toblog\.ctobsnssdk\.com\/service\/2\/app_log\/ url script-response-body jingweixiangji.js
+https:\/\/toblog\.ctobsnssdk\.com\/service\/2\/app_log\/ url script-response-body https://raw.githubusercontent.com/layfu/filter/main/jingweixiangji.js
 MITM = toblog.ctobsnssdk.com
 */
 const gettime = "/2/app_log/";

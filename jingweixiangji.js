17ebf99ff2d0623aa060bba338bd39c2994a1bd6
layfu <60004939+layfu@users.noreply.github.com>
2021-06-17 22:25:42 +0800

    Update jingweixiangji.js

--- jingweixiangji.js
+++ jingweixiangji.js
@@ -4,11 +4,7 @@ QX:
 https:\/\/toblog\.ctobsnssdk\.com\/service\/2\/app_log\/ url script-response-body https://raw.githubusercontent.com/layfu/filter/main/jingweixiangji.js
 MITM = toblog.ctobsnssdk.com
 */
-const gettime = "/2/app_log/";
 var body = $response.body;
-
-if ($request.url.indexOf(gettime) != -1) {
         var obj = JSON.parse(body);
 	obj.server_time = 1623937315;
-}
 $done({body: JSON.stringify(obj)});

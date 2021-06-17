952c59015d58ae31ba1f290058a47d4ca93c0918
layfu <60004939+layfu@users.noreply.github.com>
2021-06-17 22:08:31 +0800

    Update jingweixiangji.js

--- jingweixiangji.js
+++ jingweixiangji.js
@@ -8,6 +8,6 @@ const gettime = "/2/app_log/";
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(gettime) != -1) {
-	obj.server_time = "1623937315";
+	obj.server_time = 1623937315;
 }
-$done({body: JSON.stringify(boj)});
+$done({body: JSON.stringify(obj)});

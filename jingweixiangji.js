144553854b907bd61a2cc0634a3c7c5f7a0a1725
layfu <60004939+layfu@users.noreply.github.com>
2021-06-17 21:54:37 +0800

    Add files via upload

---
+++ jingweixiangji.js
@@ -0,0 +1,13 @@
+/*
+yuandaojingweixiangji Chain breaker
+QX:
+https:\/\/toblog\.ctobsnssdk\.com\/service\/2\/app_log\/ url script-response-body jingweixiangji.js
+MITM = toblog.ctobsnssdk.com
+*/
+const gettime = "/2/app_log/";
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(gettime) != -1) {
+	obj.server_time = 1623937315;
+}
+$done({body: JSON.stringify(boj)});
